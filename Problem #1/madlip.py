672d97b4bcb29ccaffd7e77a26eb49b832ce7509
edugo0627 <ejv0065@utulsa.edu>
2024-03-07 21:36:44 -0600

    problem 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,9 +1,20 @@
+'''
+Name: Emma Verdugo
+lab: @2pm Thursday
+
+'''
 def food_input():
     user_input = input()
     tokens = user_input.split()
     #type you while  loop here
-
+    word = (tokens[0])
+    num = (tokens[1])
     
-
+    while word != 'quit':
+       print( "Eating " + num +" " + word + " a day keeps you happy and healthy.")
+       user_input = input()
+       tokens = user_input.split()
+       word = (tokens[0])
+       num = (tokens[1])
 if __name__ == "__main__":
     food_input()
